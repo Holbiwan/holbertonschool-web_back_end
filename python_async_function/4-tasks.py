b9a9a9c1b5b2625049ed4ab5ea2456c19fc0133d
Holbiwan <sabrinapapeau@gmail.com>
2024-01-03 18:55:18 +0100

    Fix errors

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-"""Module returns list of delays using asyncio.Tasks and task_wait_random"""
+"""Module returns list of delays using asyncio tasks and task_wait_random"""
 
 import asyncio
 from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Returns list of delays using asyncio.Tasks and task_wait_random."""
+    """Returns list of delays using asyncio tasks and task_wait_random"""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
-    return await asyncio.gather(*tasks)
+    delayed_tasks = [await task for task in asyncio.as_completed(tasks)]
+    return delayed_tasks

6ec0f5c927b0ceed26dce7d2e29fc6a876b5ae39
Holbiwan <sabrinapapeau@gmail.com>
2024-01-03 17:07:33 +0100

    Create Fix error in definition of function

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,7 @@ Spawns wait_random n times, returns delays in ascending order
 from typing import List
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def wait_random(n: int, max_delay: int) -> List[float]:
     """Returns list of delays in ascending order."""
     delays = [await wait_random(max_delay) for _ in range(n)]
     return sorted(delays)

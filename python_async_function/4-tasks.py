468128b8836ea11dc59ddec425ae6510b9ddbb96
Holbiwan <sabrinapapeau@gmail.com>
2024-01-03 18:47:29 +0100

    Fix review all  code

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 """Module returns list of delays using asyncio.Tasks and task_wait_random"""
 
-
 import asyncio
 from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
-
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Returns list of delays using asyncio.Tasks and task_wait_random."""

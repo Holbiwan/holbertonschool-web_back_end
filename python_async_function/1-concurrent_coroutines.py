3826715897890b2aaec81f6d589f38cbff9a0741
Holbiwan <sabrinapapeau@gmail.com>
2024-01-03 17:19:28 +0100

    Re-built code structure

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-"""
-Async 'wait_n': int 'n', int 'max_delay' -> List[float]
-Spawns wait_random n times, returns delays in ascending order
-"""
-
+"""Coroutines Async and Python"""
+import asyncio
 from typing import List
 
+wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_random(n: int, max_delay: int) -> List[float]:
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Returns list of delays in ascending order."""
     delays = [await wait_random(max_delay) for _ in range(n)]
     return sorted(delays)

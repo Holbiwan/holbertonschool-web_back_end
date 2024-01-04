28b23b00ea1dc40b8e5793c08854c8f0e4f8ea5f
(Saby)  HOLBIWAN <sabrinapapeau@gmail.com>
2024-01-04 22:18:57 +0100

    Update 2-measure_runtime.py

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -8,7 +8,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Function to coutne"""
+    """Function to count"""
     start = time.perf_counter()
     await asyncio.gather(async_comprehension(), async_comprehension(),
                          async_comprehension(), async_comprehension())

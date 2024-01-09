0c6a2526a4c4adc4ed7a67b7a4a99799a66be8b2
Holbiwan <sabrinapapeau@gmail.com>
2024-01-09 16:11:48 +0100

    Replace count() method  by count-_document() method

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -6,23 +6,22 @@ from pymongo import MongoClient
 
 def count_all(mongo_collection):
     """counts all documents in a collection"""
-    return mongo_collection.count()
+    return mongo_collection.count_documents({})
 
 
 def count_method(mongo_collection, method):
     """counts all documents in a collection with a particular method"""
-    return mongo_collection.count({"method": method})
+    return mongo_collection.count_documents({"method": method})
 
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx = client.logs.nginx
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-
     print("{} logs".format(count_all(nginx)))
     print("Methods:")
     for method in methods:
         print("\tmethod {}: {}".format(method, count_method(nginx, method)))
 
     print("{} status check".format(
-        nginx.count({"method": "GET", "path": "/status"})))
+        count_method(nginx, {"method": "GET", "path": "/status"})))

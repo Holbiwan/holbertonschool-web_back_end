c5b7fbeb6c95466b8c1aa9214767938f0b5ba28f
HOLBIWAN (Saby) <sabrinapapeau@gmail.com>
2024-01-10 02:23:33 +0100

    Remove duplicate method

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-"""log stats from collection
-"""
+"""log stats"""
+
 from pymongo import MongoClient
 
 
@@ -8,8 +8,8 @@ METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
 
 def log_stats(mongo_collection, option=None):
-    """ script that provides some stats about Nginx logs stored in MongoDB
-    """
+    """provides stats about Nginx logs stored in MongoDB"""
+    
     items = {}
     if option:
         value = mongo_collection.count_documents(

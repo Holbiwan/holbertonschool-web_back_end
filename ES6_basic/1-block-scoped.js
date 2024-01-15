5edfa06d9e44bc7996bcd81d267deccf1b8e9973
Holbiwan <sabrinapapeau@gmail.com>
2024-01-15 22:59:14 +0100

    Remove localTask part and let function

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,19 +2,12 @@ export default function taskBlock(trueOrFalse) {
     const task = false;
     const task2 = true;
   
-    let localTask = task;
-    let localTask2 = task2;
-  
+    // eslint-disable-next-line
     if (trueOrFalse) {
-      // eslint-disable-next-line
-      localTask = true;
-      localTask2 = false;
-  
-      // Returns values of local variables inside the block   
-      return [localTask, localTask2];
+      const task = true;
+      const task2 = false;
     }
+    // eslint-disable-no-unused-vars
   
-    // Returns values of variables declared outside the block    
     return [task, task2];
   }
-  

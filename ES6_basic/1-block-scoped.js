da3eeb19700831160b366fa1fa825f4c70a7ae40
Holbiwan <sabrinapapeau@gmail.com>
2024-01-15 23:29:18 +0100

    Review code with npx eslint --fix

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,15 +1,15 @@
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
-  
+  const task = false;
+  const task2 = true;
+
+  // eslint-disable-next-line no-unused-vars
+  if (trueOrFalse) {
+    const localTask = true;
+    const localTask2 = false;
+
     // eslint-disable-next-line no-unused-vars
-    if (trueOrFalse) {
-      const localTask = true;
-      const localTask2 = false;
-  
-      // eslint-disable-next-line no-unused-vars
-      return [localTask, localTask2];
-    }
-  
-    return [task, task2];
+    return [localTask, localTask2];
   }
+
+  return [task, task2];
+}

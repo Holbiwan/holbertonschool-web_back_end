8d1c4d6b17d9f38dcf1d98c17de873fde6ccc5b0
Holbiwan <sabrinapapeau@gmail.com>
2024-01-15 23:14:27 +0100

    Back to locasl Task following eslint verification

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,12 +2,14 @@ export default function taskBlock(trueOrFalse) {
     const task = false;
     const task2 = true;
   
-    // eslint-disable-next-line
+    // eslint-disable-next-line no-unused-vars
     if (trueOrFalse) {
-      const task = true;
-      const task2 = false;
+      const localTask = true;
+      const localTask2 = false;
+  
+      // eslint-disable-next-line no-unused-vars
+      return [localTask, localTask2];
     }
-    // eslint-disable-no-unused-vars
   
     return [task, task2];
-}
+  }

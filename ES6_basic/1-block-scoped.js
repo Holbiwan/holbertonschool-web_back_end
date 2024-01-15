8384b0cf981fec821832e1811af35f6c7a0b8f3c
Holbiwan <sabrinapapeau@gmail.com>
2024-01-16 00:27:57 +0100

    Modify comments style

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,12 +2,11 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  // eslint-disable-next-line no-unused-vars
   if (trueOrFalse) {
+    /* eslint-disable no-unused-vars */
     const task = true;
     const task2 = false;
+    /* eslint-enable no-unused-vars */
   }
-  // eslint-disable-next-line no-unused-vars
-
   return [task, task2];
 }

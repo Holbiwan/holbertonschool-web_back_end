9c920f8b390a186bdde8bb39a36320fb0bb250f6
Holbiwan <sabrinapapeau@gmail.com>
2024-01-15 22:53:48 +0100

    Try to use let instead of const on the localTask

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,15 +1,20 @@
 export default function taskBlock(trueOrFalse) {
     const task = false;
     const task2 = true;
-
+  
+    let localTask = task;
+    let localTask2 = task2;
+  
     if (trueOrFalse) {
-        // eslint-disable-next-line
-        const localTask = true;
-        const localTask2 = false;
-
-       // Returns values of local variables inside the block   
-        return [localTask, localTask2];
+      // eslint-disable-next-line
+      localTask = true;
+      localTask2 = false;
+  
+      // Returns values of local variables inside the block   
+      return [localTask, localTask2];
     }
-       // Returns values of variables declared outside the block    
-      return [task, task2];
-    }
+  
+    // Returns values of variables declared outside the block    
+    return [task, task2];
+  }
+  

cc2dd994e1b2f58195c8f2f7328c5bdcc74f4377
Holbiwan <sabrinapapeau@gmail.com>
2024-01-24 17:31:05 +0100

    Remove condition

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,9 +1,7 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    if (true) {
+    return new Promise((resolve, reject) => {
       resolve();
-    } else {
       reject();
-    }
-  });
-}
+    });
+  }
+  

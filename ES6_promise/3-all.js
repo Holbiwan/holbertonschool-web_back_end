0920e6dd2367c90174670550ace24164cb2e4fbf
Holbiwan <sabrinapapeau@gmail.com>
2024-01-25 23:43:17 +0100

    Create 3-all.js file

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+
+function handleProfileSignup() {
+  const photoPromise = uploadPhoto();
+  const userPromise = createUser();
+
+  Promise.all([photoPromise, userPromise])
+    .then((results) => {
+      const [photo, user] = results;
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
+
+export default handleProfileSignup;

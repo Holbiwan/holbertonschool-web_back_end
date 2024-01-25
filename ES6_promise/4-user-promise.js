bba8507db4890b4662975a8d095ba5aab2834edb
Holbiwan <sabrinapapeau@gmail.com>
2024-01-26 00:40:33 +0100

    Remove comment at the beginning of code

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,11 +1,8 @@
-// Sign up user
 function signUpUser(firstName, lastName) {
-    return Promise.resolve(
-      {
-        firstName,
-        lastName,
-      },
-    );
-  }
-  
-  export default signUpUser;
+  return Promise.resolve({
+    firstName,
+    lastName,
+  });
+}
+
+export default signUpUser;

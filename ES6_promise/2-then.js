0b22b22fdd2d101456273dfec50b76c3c3dd598f
Holbiwan <sabrinapapeau@gmail.com>
2024-01-25 23:06:55 +0100

    Correct syntax

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,9 +1,12 @@
 export default function handleResponseFromAPI(promise) {
- return promise.then(() => {
-   console.log('Got a response from the API');
-   return { status: 200, body: 'success' };
- }).catch(() => {
-   console.log('Got a response from the API');
-   throw new Error();
- });
-}
+    return promise
+      .then(() => {
+        console.log('Got a response from the API');
+        return { status: 200, body: 'success' };
+      })
+      .catch(() => {
+        console.log('Got a response from the API');
+        throw new Error(); // Utilisez 'throw' pour rejeter la promesse avec une erreur
+      });
+  }
+  

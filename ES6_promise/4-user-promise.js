30b5add8a9cb84c80b520a80e5ff29f331559bfd
Holbiwan <sabrinapapeau@gmail.com>
2024-01-26 00:31:56 +0100

    Fix indentation

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,12 +1,10 @@
 // Register a user
 function signUpUser(firstName, lastName) {
-    return Promise.resolve(
-    {
-    firstName,
-    lastName,
-    },
-    );
-    }
-    
-    export default signUpUser;
-    
+    return Promise.resolve({
+      firstName,
+      lastName,
+    });
+  }
+  
+  export default signUpUser;
+  

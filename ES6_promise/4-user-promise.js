68c7c72e76b942ecaf0b0c6840ecf834898e971f
Holbiwan <sabrinapapeau@gmail.com>
2024-01-26 00:30:02 +0100

    Create 4-user-promise.js file

---
+++ ES6_promise/4-user-promise.js
@@ -0,0 +1,12 @@
+// Register a user
+function signUpUser(firstName, lastName) {
+    return Promise.resolve(
+    {
+    firstName,
+    lastName,
+    },
+    );
+    }
+    
+    export default signUpUser;
+    

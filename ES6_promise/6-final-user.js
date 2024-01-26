945031cdbecb37e2527170dfcd3b44ed00e17018
Holbiwan <sabrinapapeau@gmail.com>
2024-01-26 10:11:07 +0100

    Fix eslint issues

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,30 +4,30 @@ import uploadPhoto from './5-photo-reject';
 
 // Define an asynchronous function 'handleProfileSignup'
 async function handleProfileSignup(firstName, lastName, fileName) {
-    // Helper function to create a promise
+  // Helper function to create and handle promises
   const createPromise = async (promise, asyncFn) => {
     try {
-        // Try to await the asynchronous function
+      // Try to await the asynchronous function
       promise.value = await asyncFn();
       promise.status = 'fulfilled';
     } catch (err) {
-        // If the asynchronous function throws an error, catch it
+      // If there's an error, handle it and mark the promise as rejected
       promise.value = err.toString();
       promise.status = 'rejected';
     }
   };
 
-  // Create two promises
+  // Define two promises with 'pending' status
   const promise1 = { status: 'pending' };
   const promise2 = { status: 'pending' };
 
-  // Await the asynchronous functions
+  // Create promises for signUpUser and uploadPhoto functions
   await createPromise(promise1, () => signUpUser(firstName, lastName));
   await createPromise(promise2, () => uploadPhoto(fileName));
 
-    // Return the promises
+  // Return an array containing the results of both promises
   return [promise1, promise2];
 }
 
-// Export the function 'handleProfileSignup'
+// Export the 'handleProfileSignup' function as the default export
 export default handleProfileSignup;

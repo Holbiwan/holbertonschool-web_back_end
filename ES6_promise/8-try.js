f0bcaf419965ddf3fadbfb3fadb47149dc589393
Holbiwan <sabrinapapeau@gmail.com>
2024-01-26 12:19:23 +0100

    Add comment at the top of code

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,7 +1,7 @@
+// Throw an error if denominator is equal to 0 
 export default function divideFunction(numerator, denominator) {
     if (denominator === 0) {
       throw new Error('cannot divide by 0');
     }
     return numerator / denominator;
-  }
-  
+  }  

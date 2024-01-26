a653ca5dc1ceef0c7ec502561be3587a87be7ee6
Holbiwan <sabrinapapeau@gmail.com>
2024-01-26 10:56:53 +0100

    Fix eslint

--- ES6_promise/7-load_balancer.js
+++ ES6_promise/7-load_balancer.js
@@ -1,4 +1,4 @@
-// Race to look which is the first
+// Determine the first resolved or rejected promise
 export default function loadBalancer(chinaDownload, USDownload) {
     return Promise.race([chinaDownload, USDownload]);
-  }
+}

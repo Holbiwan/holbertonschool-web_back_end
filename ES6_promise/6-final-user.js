1239df0cba342f50e062ff2e84fa84042a6c39f7
Holbiwan <sabrinapapeau@gmail.com>
2024-01-26 10:13:33 +0100

    Back to previous code without commments

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,33 +1,33 @@
-// Importing necessary functions from external modules
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-// Define an asynchronous function 'handleProfileSignup'
 async function handleProfileSignup(firstName, lastName, fileName) {
-  // Helper function to create and handle promises
-  const createPromise = async (promise, asyncFn) => {
-    try {
-      // Try to await the asynchronous function
-      promise.value = await asyncFn();
-      promise.status = 'fulfilled';
-    } catch (err) {
-      // If there's an error, handle it and mark the promise as rejected
-      promise.value = err.toString();
-      promise.status = 'rejected';
-    }
+  const promise1 = {
+    status: 'pending ',
+  };
+  const promise2 = {
+    status: 'pending ',
   };
 
-  // Define two promises with 'pending' status
-  const promise1 = { status: 'pending' };
-  const promise2 = { status: 'pending' };
+  try {
+    const signup = await signUpUser(firstName, lastName);
+    promise1.status = 'fulfilled';
+    promise1.value = signup;
+  } catch (err) {
+    promise1.status = 'rejected';
+    promise1.value = err.toString();
+  }
 
-  // Create promises for signUpUser and uploadPhoto functions
-  await createPromise(promise1, () => signUpUser(firstName, lastName));
-  await createPromise(promise2, () => uploadPhoto(fileName));
+  try {
+    const upload = await uploadPhoto(fileName);
+    promise2.status = 'fulfilled';
+    promise2.value = upload;
+  } catch (err) {
+    promise2.status = 'rejected';
+    promise2.value = err.toString();
+  }
 
-  // Return an array containing the results of both promises
   return [promise1, promise2];
 }
 
-// Export the 'handleProfileSignup' function as the default export
 export default handleProfileSignup;

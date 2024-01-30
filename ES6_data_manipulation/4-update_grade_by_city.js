14a94f2315a092ae15d2abc4273da71de2bc506e
Holbiwan <sabrinapapeau@gmail.com>
2024-01-30 21:05:26 +0100

    Fix eslint errors

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,9 @@
-const updateStudentGradeByCity = (students, city, newGrade) =>
-  students.map((student) => ({
-    ...student,
-    grade: (newGrade.find((grade) => grade.studentId === student.id) || {}).grade || 'N/A',
-  })).filter((student) => student.location === city);
+const updateStudentGradeByCity = (students, city, newGrades) =>
+  students
+    .map((student) => ({
+      ...student,
+      grade: (newGrades.find((grade) => grade.studentId === student.id) || {}).grade || 'N/A',
+    }))
+    .filter((student) => student.location === city);
 
 export default updateStudentGradeByCity;

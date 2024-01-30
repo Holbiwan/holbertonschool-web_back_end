65f0df1d996a2348a6d6cad52d53606aeb292b2d
Holbiwan <sabrinapapeau@gmail.com>
2024-01-30 09:03:15 +0100

    Import getListStudents from task 0

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,8 @@
 // Use reduce to return the sum of the ids
+import getListStudents from "./0-get_list_students.js";
+
 function getStudentIdsSum(students) {
-    return students.reduce((sum, student) => sum + student.id, 0);
-  }
-  
-  export default getStudentIdsSum;
-  
+  return students.reduce((sum, student) => sum + student.id, 0);
+}
+
+export default getStudentIdsSum;

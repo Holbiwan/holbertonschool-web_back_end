e4663818e9503950f8a6cc7cae790102e2df4e57
Holbiwan <sabrinapapeau@gmail.com>
2024-01-30 23:45:50 +0100

    Back to previous codestyle adding module

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,13 @@
 const updateUniqueItems = (map) => {
-  if (!(map instanceof Map)) throw TypeError('Cannot process');
-    map.forEach((value, key) => {
-        if (value === 1) map.set(key, 100);
-    }); 
-
-export default updateUniqueItems;
+    if (!(map instanceof Map)) {
+      throw new Error('Cannot process');
+    }
+  
+    for (const [key, value] of map.entries()) {
+      if (value === 1) {
+        map.set(key, 100);
+      }
+    }
+  };
+  
+  module.exports = updateUniqueItems;

0fbd688543fb4d80f2b3a88f47f978ef4c229f44
Holbiwan <sabrinapapeau@gmail.com>
2024-01-30 09:06:46 +0100

    Try a shorter version of code

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,8 +1,3 @@
-// Use reduce to return the sum of the ids
-import getListStudents from "./0-get_list_students.js";
-
-function getStudentIdsSum(students) {
-  return students.reduce((sum, student) => sum + student.id, 0);
-}
+const getStudentIdsSum = (list) => list.reduce((total, student) => total + student.id, 0);
 
 export default getStudentIdsSum;

0c0f9a1a3e84634866a223fae71493e1d56ea588
Holbiwan <sabrinapapeau@gmail.com>
2024-01-30 01:31:51 +0100

    Fix indentation and eslint errors

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,10 +2,7 @@ function getListStudentIds(students) {
   if (!Array.isArray(students)) {
     return [];
   }
-
   const ids = students.map((item) => item.id);
-
   return ids;
 }
-
 export default getListStudentIds;  

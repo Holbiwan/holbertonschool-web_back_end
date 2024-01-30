e0d5ff18ae475c14d7b55ca59768503498d0b33c
Holbiwan <sabrinapapeau@gmail.com>
2024-01-30 02:51:53 +0100

    Adding comments on top of code

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,7 @@
+// getStudentsByLocation function filters an array of students
+// based on the specified city and returns the filtered array.
 const getStudentsByLocation = (students, city) => {
-    return students.filter(student => student.location === city);
-  };
-  
-  export default getStudentsByLocation;
-  
+  return students.filter(student => student.location === city);
+};
+
+export default getStudentsByLocation;  

ee8fb069c7d204f7caa45b2e5ce2e7b916779eb8
Holbiwan <sabrinapapeau@gmail.com>
2024-01-30 01:08:31 +0100

    Create 1-get_list_student_ids.js file

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+function getListStudents(students) {
+    if (!Array.isArray(students)) {
+        return [];
+    }
+    return students.map((student) => student.id);
+    }
+
+    export default getListStudents;

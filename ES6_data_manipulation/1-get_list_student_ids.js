880c0a6e1eab90e7abbe4ebcc0592833d582afad
Holbiwan <sabrinapapeau@gmail.com>
2024-01-30 01:14:07 +0100

    Fix syntax issue

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,7 +2,8 @@ function getListStudents(students) {
     if (!Array.isArray(students)) {
         return [];
     }
-    return students.map((student) => student.id);
-    }
 
-    export default getListStudents;
+    return students.map((student) => student.id);
+}
+
+export default getListStudents;

1e221c1e1c9830f0cc2f511f8f55fdf658fad429
Holbiwan <sabrinapapeau@gmail.com>
2024-01-30 03:43:34 +0100

    Add missing braces

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,9 @@
 // Use reduce to return the sum of the ids
+
 function getStudentIdsSum(students) {
-    return students.reduce((sum, student) => sum + student.id, 0);
+    return students.reduce((sum, student) => {
+      return sum + student.id;
+    }, 0);
   }
   
   export default getStudentIdsSum; 

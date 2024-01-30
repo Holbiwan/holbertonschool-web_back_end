37835abd4fae3b2826b87f144f114e06388f10dc
Holbiwan <sabrinapapeau@gmail.com>
2024-01-30 03:09:46 +0100

    Back to previous code

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,9 +1,11 @@
-export default function getStudentsByLocation(students, city) {
-  console.log("Received list of students:", students);
-  
-  const studentsInCity = students.filter(student => student.location === city);
-  
-  console.log("Students in the specific city:", studentsInCity);
-  
-  return studentsInCity;
-}
+// Filter based in the search of the city
+
+const getStudentsByLocation = (students, city) => {
+  const studentsLocation = students.filter(
+    (student) => student.location === city,
+  );
+
+  return studentsLocation;
+};
+
+export default getStudentsByLocation;

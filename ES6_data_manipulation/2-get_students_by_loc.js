387842ae37752d25df3ac0cf0323406a7d0faf51
Holbiwan <sabrinapapeau@gmail.com>
2024-01-30 01:59:12 +0100

    Fix eslint issues

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,5 @@
 export default function getStudentsByLocation(students, city) {
-const studentsInCity = students.filter((student) => student.location === city);
-return studentsInCity;
-}
+    const studentsInCity = students.filter((student) => student.location === city);
+    return studentsInCity;
+  }
+  

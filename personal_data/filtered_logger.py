51719655194f6620047653b07be2cd8536ef7779
Holbiwan <sabrinapapeau@gmail.com>
2024-06-18 16:51:24 +0200

    Handle Log formatter class

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Regex-ing"""
 
+import logging
 import re
 from typing import List
 
@@ -15,3 +16,36 @@ def filter_datum(fields: List[str], redaction: str,
             message
         )
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = ("[HOLBERTON] %(name)s %(levelname)s "
+              "%(asctime)-15s: %(message)s")
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Filter values in incoming log records using filter_datum """
+        record.msg = filter_datum(
+            self.fields, self.REDACTION,
+            record.getMessage(), self.SEPARATOR
+        )
+        return super().format(record)
+
+
+if __name__ == '__main__':
+    # Example usage
+    message = (
+        "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
+    )
+    log_record = logging.LogRecord(
+        "my_logger", logging.INFO, None, None, message, None, None
+    )
+    formatter = RedactingFormatter(fields=("email", "ssn", "password"))
+    print(formatter.format(log_record))

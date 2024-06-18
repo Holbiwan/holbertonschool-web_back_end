161835b2599f8d549947488ca7c06ed3ef3eaa2b
Holbiwan <sabrinapapeau@gmail.com>
2024-06-18 07:04:21 +0200

    Create filter_datum function

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Regex-ing"""
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Filter data in log message."""
+    for field in fields:
+        message = re.sub(
+            f"{field}=.*?{separator}",
+            f"{field}={redaction}{separator}",
+            message
+        )
+    return message

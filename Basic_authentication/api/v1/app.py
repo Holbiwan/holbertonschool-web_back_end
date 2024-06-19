8ea2ecd7009359e9686ac94d2ede55441114df5a
Holbiwan <sabrinapapeau@gmail.com>
2024-06-20 00:29:38 +0200

    Fix errorHandler

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -18,10 +18,10 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
 AUTH_TYPE = getenv("AUTH_TYPE")
-if AUTH_TYPE == 'auth':
-    auth = Auth()
-elif AUTH_TYPE == 'basic_auth':
+if AUTH_TYPE == "basic_auth":
     auth = BasicAuth()
+else:
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -29,6 +29,36 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ unauthorized handler """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Forbidden handler """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """before_request function"""
+    if auth is None:
+        return
+    exist = auth.require_auth(
+        request.path,
+        ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'])
+    if exist is False:
+        return
+    authorization_header = auth.authorization_header(request)
+    if authorization_header is None:
+        abort(401)
+    current_user = auth.current_user(request)
+    if current_user is None:
+        abort(403)
 
 
 if __name__ == "__main__":

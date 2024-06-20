a647355c8969a424ecce669117a3b0fe3172a507
Holbiwan <sabrinapapeau@gmail.com>
2024-06-20 02:20:58 +0200

    Fix issues in code

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -7,21 +7,20 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
 auth = None
-
 AUTH_TYPE = getenv("AUTH_TYPE")
-if AUTH_TYPE == "basic_auth":
-    auth = BasicAuth()
-else:
+
+if AUTH_TYPE == "auth":
+    from api.v1.auth.auth import Auth
     auth = Auth()
+elif AUTH_TYPE == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
@@ -32,32 +31,39 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> str:
-    """ unauthorized handler """
+def unauthorized_error(error) -> str:
+    """ Unauthorized handler
+    """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error) -> str:
-    """ Forbidden handler """
+def forbidden_error(error) -> str:
+    """ Forbidden handler
+    """
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
-def before_request():
-    """before_request function"""
+def before_request() -> str:
+    """ Before Request Handler
+
+    Requests Validation
+    """
     if auth is None:
         return
-    exist = auth.require_auth(
-        request.path,
-        ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'])
-    if exist is False:
+
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+
+    if not auth.require_auth(request.path, excluded_paths):
         return
-    authorization_header = auth.authorization_header(request)
-    if authorization_header is None:
+
+    if auth.authorization_header(request) is None:
         abort(401)
-    current_user = auth.current_user(request)
-    if current_user is None:
+
+    if auth.current_user(request) is None:
         abort(403)
 
 

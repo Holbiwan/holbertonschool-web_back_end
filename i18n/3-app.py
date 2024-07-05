71d1a8d9e02e27abced4a3f27e5769c8bd226f9c
Holbiwan <sabrinapapeau@gmail.com>
2024-07-05 17:31:09 +0200

    Fix attributr Error Babel

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,34 +1,25 @@
-#!/usr/bin/env python3
-""" Task 3: Parametrize templates """
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
 app = Flask(__name__)
+
+# Configuration de Babel
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_TRANSLATION_DIRECTORIES'] = 'translations'
+
 babel = Babel(app)
 
 
-class Config(object):
-    """Class to configure available languages in the app"""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-app.config.from_object(Config)
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(['en', 'fr'])
 
 
 @app.route('/')
 def index():
 
-    """Returning our html page"""
     return render_template('3-index.html')
 
 
-@babel.localeselector
-def get_locale():
-    """Getting locale from request.accept_languages"""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(debug=True)

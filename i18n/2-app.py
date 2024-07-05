43c31beaa20567c5d53af8704690f6837b2c9737
Holbiwan <sabrinapapeau@gmail.com>
2024-07-05 16:25:10 +0200

    Pycodestyle review

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -3,13 +3,11 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-
 app = Flask(__name__)
-babel = Babel(app)
 
 
 class Config(object):
-    """ Class will configure available languages in the app """
+    """Class to configure available languages in the app"""
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -18,15 +16,19 @@ class Config(object):
 app.config.from_object(Config)
 
 
+babel = Babel(app)
+
+
 @app.route('/')
 def index():
-    """ Returning our html page """
+
+    """Returning our html page"""
     return render_template('2-index.html')
 
 
 @babel.localeselector
 def get_locale():
-    """ Getting locale from request.accept_languages """
+    """Getting locale from request.accept_languages"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 

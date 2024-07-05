17804f94b00560a472ea759a117a8def13d8d2b2
Holbiwan <sabrinapapeau@gmail.com>
2024-07-05 16:43:47 +0200

    Pycodestyle review

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
 """ Task 3: Parametrize templates """
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
-
+from flask_babel import Babel, _
 
 app = Flask(__name__)
 babel = Babel(app)
-gettext.__doc__ = "Documentation for gettext"
-""" Checker requirements """
 
 
 class Config(object):
-    """ Class will configure available languages in the app """
+    """Class to configure available languages in the app"""
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -22,13 +19,14 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    """ Returning our html page """
+
+    """Returning our html page"""
     return render_template('3-index.html')
 
 
 @babel.localeselector
 def get_locale():
-    """ Getting locale from request.accept_languages """
+    """Getting locale from request.accept_languages"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 

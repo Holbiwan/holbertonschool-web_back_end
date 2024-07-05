308e39285b8c77cd3e111830d932c5dae55f032d
Holbiwan <sabrinapapeau@gmail.com>
2024-07-05 18:35:15 +0200

    Modify syntax

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
+
 """ Setup a basic Flask app """
 
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
-@app.route('/')
-def index():
-    """Returning to html page """
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_world() -> str:
+    """Renders a Basic Template for Babel Implementation"""
     return render_template("0-index.html")
 
 

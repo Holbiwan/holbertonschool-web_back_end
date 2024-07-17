d2bd32e882406566feb91c730a05c5c23b987113
Holbiwan <sabrinapapeau@gmail.com>
2024-07-17 09:08:00 +0200

    Clean directory

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Module for testing client """
+""" Module for testing client """
 
 import unittest
 from parameterized import parameterized, parameterized_class
--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""File for testing utils.py"""
+""" File for testing utils.py """
 
 import unittest
 from unittest.mock import patch, Mock
@@ -8,7 +8,7 @@ from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Test Access Nested Map Class"""
+    """ Test Access Nested Map Class """
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -16,7 +16,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, map, path, expected_output):
-        """Test Access Nested Map Method"""
+        """ Test Access Nested Map Method """
         real_output = access_nested_map(map, path)
         self.assertEqual(real_output, expected_output)
 
@@ -25,21 +25,21 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b"), 'b')
     ])
     def test_access_nested_map_exception(self, map, path, wrong_output):
-        """Test Access Nested Map Exception Method"""
+        """ Test Access Nested Map Exception Method """
         with self.assertRaises(KeyError) as e:
             access_nested_map(map, path)
             self.assertEqual(wrong_output, e.exception)
 
 
 class TestGetJson(unittest.TestCase):
-    """Test Get JSON Class"""
+    """ Test Get JSON Class """
 
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False})
     ])
     def test_get_json(self, test_url, test_payload):
-        """Test Get JSON Method"""
+        """ Test Get JSON Method """
         mock_response = Mock()
         mock_response.json.return_value = test_payload
         with patch('requests.get', return_value=mock_response):
@@ -49,21 +49,21 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
-    """Test Memoize Class"""
+    """ Test Memoize Class """
 
     def test_memoize(self):
-        """Test Memoize Method"""
+        """ Test Memoize Method """
 
         class TestClass:
-            """Test Class"""
+            """ Test Class """
 
             def a_method(self):
-                """A Method"""
+                """ A Method """
                 return 42
 
             @memoize
             def a_property(self):
-                """A Property Method"""
+                """ A Property Method """
                 return self.a_method()
 
         with patch.object(TestClass, 'a_method', return_value=42) as patched:

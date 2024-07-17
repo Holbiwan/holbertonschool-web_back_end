c32ea3f0a10ede3de73ff65dd22585805c9e0a84
Holbiwan <sabrinapapeau@gmail.com>
2024-07-17 17:43:03 +0200

    Fix issue and eslint

--- unittests_in_js/8-api/api.js
+++ unittests_in_js/8-api/api.js
@@ -1,11 +1,12 @@
-/* eslint-disable */
 const express = require('express');
+
 const app = express();
+const port = 7865;
 
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.listen(7865, () => {
+app.listen(port, () => {
   console.log('API available on localhost port 7865');
 });

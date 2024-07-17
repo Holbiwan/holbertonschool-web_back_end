0ef2b4175a7612f96ff5fbc760ae30409f116287
Holbiwan <sabrinapapeau@gmail.com>
2024-07-17 15:51:38 +0200

    Fix eslint install issues

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,22 +1,36 @@
-/* 0-calcul.test.js */
-
+/* eslint-disable */
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function() {
-  it('should return 4 when inputs are 1 and 3', function() {
+describe('calculateNumber', () => {
+  it('should return an int number', () => {
+    assert.strictEqual(typeof calculateNumber(2.3, 3.1), 'number');
+  });
+  it('should return sum of num', () => {
+    assert.strictEqual(calculateNumber(2.2, 2, 3), 4);
+  });
+  it('should return sum', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
-
-  it('should return 5 when inputs are 1 and 3.7', function() {
+  it('sum of nums', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
-
-  it('should return 5 when inputs are 1.2 and 3.7', function() {
+  it('return sum of nums', () => {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
-
-  it('should return 6 when inputs are 1.5 and 3.7', function() {
+  it('sum', () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+  it('sumNums', () => {
+    assert.strictEqual(calculateNumber(-2, -2), -4);
+  });
+  it('sum negative', () => {
+    assert.strictEqual(calculateNumber(-1.3, -4.5), -5);
+  });
+  it('sum decimals', () => {
+    assert.strictEqual(calculateNumber(0.4, 0.6), 1);
+  });
+  it('sum decimal', () => {
+    assert.strictEqual(calculateNumber(0.4, 0.2), 0);
   });
 });

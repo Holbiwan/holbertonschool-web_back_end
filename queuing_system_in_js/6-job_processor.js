ce713e1fee13be842bae9db4483a20b4dba7d177
Holbiwan <sabrinapapeau@gmail.com>
2024-07-19 14:48:19 +0200

    Added try-catch block around function sendNotification

--- queuing_system_in_js/6-job_processor.js
+++ queuing_system_in_js/6-job_processor.js
@@ -1,17 +1,22 @@
 import kue from 'kue';
 
+// Create the queue
 const queue = kue.createQueue();
 
 const queueName = 'push_notification_code';
 
 const sendNotification = (phoneNumber, message) => {
-  console.log(
-    `Sending notification to ${phoneNumber}, with message: ${message}`
-  );
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
 };
 
 queue.process(queueName, (job, done) => {
   const { phoneNumber, message } = job.data;
-  sendNotification(phoneNumber, message);
-  done();
+
+  try {
+    sendNotification(phoneNumber, message);
+    done(); // Signal job completion success
+  } catch (error) {
+    console.error(`Failed to send notification: ${error.message}`);
+    done(error); // Signal job completion with error
+  }
 });

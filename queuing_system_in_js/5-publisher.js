3fd4a5e0b08fbcc39db8ff86b540f94a4d6fe089
Holbiwan <sabrinapapeau@gmail.com>
2024-07-19 14:30:44 +0200

    Add try/catch block around client.connect()

--- queuing_system_in_js/5-publisher.js
+++ queuing_system_in_js/5-publisher.js
@@ -5,7 +5,7 @@ const client = redis.createClient();
 const CHANNEL = 'holberton school channel';
 
 client.on('error', (error) => {
-  console.log(`Redis client not connected to the server: ${error.message}`);
+  console.error(`Redis client not connected to the server: ${error.message}`);
 });
 
 client.on('connect', () => {
@@ -15,13 +15,26 @@ client.on('connect', () => {
 const publishMessage = (message, time) => {
   setTimeout(() => {
     console.log(`About to send ${message}`);
-    client.publish(CHANNEL, message);
+    client.publish(CHANNEL, message, (error, reply) => {
+      if (error) {
+        console.error(`Failed to publish message: ${error.message}`);
+      } else {
+        console.log(`Message sent: ${message}, Number of subscribers that received the message: ${reply}`);
+      }
+    });
   }, time);
 };
 
-(() => {
-  publishMessage('Holberton Student #1 starts course', 100);
-  publishMessage('Holberton Student #2 starts course', 200);
-  publishMessage('KILL_SERVER', 300);
-  publishMessage('Holberton Student #3 starts course', 400);
-})();
+const run = async () => {
+  try {
+    await client.connect();
+    publishMessage('Holberton Student #1 starts course', 100);
+    publishMessage('Holberton Student #2 starts course', 200);
+    publishMessage('KILL_SERVER', 300);
+    publishMessage('Holberton Student #3 starts course', 400);
+  } catch (error) {
+    console.error(`Error connecting to Redis: ${error.message}`);
+  }
+};
+
+run();

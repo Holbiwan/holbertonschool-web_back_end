6b011010fe77b52ad09912b7a1c7ab569682d739
Holbiwan <sabrinapapeau@gmail.com>
2024-07-19 14:45:01 +0200

    Handling job ans error in the save callback

--- queuing_system_in_js/6-job_creator.js
+++ queuing_system_in_js/6-job_creator.js
@@ -1,22 +1,31 @@
 import kue from 'kue';
 
+// Create the queue
 const queue = kue.createQueue();
 
+// Define the job data
 const objectJob = {
   phoneNumber: '3167308713',
   message: 'This is the code to verify your account',
 };
 
+// Define the queue name
 const queueName = 'push_notification_code';
 
-const job = queue.create(queueName, objectJob).save((err) => {
-  if (!err) console.log(`Notification job created: ${job.id}`);
-});
+// Create and save the job
+queue.create(queueName, objectJob).save((err, job) => {
+  if (err) {
+    console.error(`Failed to create notification job: ${err}`);
+  } else {
+    console.log(`Notification job created: ${job.id}`);
 
-job.on('complete', () => {
-  console.log('Notification job completed');
-});
-
-job.on('failed', () => {
-  console.log('Notification job failed');
+    // Attach event listeners within the save callback
+    job.on('complete', () => {
+      console.log('Notification job completed');
+    }).on('failed', (errorMessage) => {
+      console.log(`Notification job failed: ${errorMessage}`);
+    }).on('progress', (progress) => {
+      console.log(`Notification job ${job.id} ${progress}% complete`);
+    });
+  }
 });

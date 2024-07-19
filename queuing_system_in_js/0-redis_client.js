b00f52c9eace27ee976a9bfe98245b514faf8f8e
Holbiwan <sabrinapapeau@gmail.com>
2024-07-19 09:52:45 +0200

    Handle the flexibility of environment

--- queuing_system_in_js/0-redis_client.js
+++ queuing_system_in_js/0-redis_client.js
@@ -1,6 +1,10 @@
 import redis from 'redis';
 
-const client = redis.createClient();
+const client = redis.createClient({
+  host: process.env.REDIS_HOST || '127.0.0.1',
+  port: process.env.REDIS_PORT || 6379,
+  retry_strategy: () => 1000,
+});
 
 client.on('error', (error) => {
   console.log(`Redis client not connected to the server: ${error.message}`);

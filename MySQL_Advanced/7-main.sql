425eb664e7db1da1359122456b43f48a28b4c5f8
Holbiwan <sabrinapapeau@gmail.com>
2024-08-09 07:55:06 +0200

    Add 7-main file

---
+++ MySQL_Advanced/7-main.sql
@@ -0,0 +1,11 @@
+-- Show and compute average score
+SELECT * FROM users;
+SELECT * FROM corrections;
+
+SELECT "--";
+CALL ComputeAverageScoreForUser((SELECT id FROM users WHERE name = "Jeanne"));
+
+SELECT "--";
+SELECT * FROM users;
+SELECT * FROM corrections;
+-- End of snippet from MySQL_Advanced/7-main.sql
